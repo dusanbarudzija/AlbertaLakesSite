40cc910366e05ac86cb300491eb476abfdbac171
BrightonGosinet <gosinet@ualberta.ca>
2026-04-03 09:54:12 -0600

    Add route to fetch all waterbodies with latest sample data

--- server/routes/waterbodies.js
+++ server/routes/waterbodies.js
@@ -4,7 +4,23 @@ const Waterbody = require('../models/Waterbody');
 const { getLatestCellCount, getLatestWaterDescription } = require('../services/samplesService');
 
 router.get('/lakes', async (req, res) => {
-    // get all waterbodies + their most recent cellCount and waterDescription (+ the dates for each) for the lakes page
+    // Get all waterbodies most recent cellCount and waterDescription (+ the dates for each) for the lakes page
+    try {
+        const waterbodies = await Waterbody.find();
+        const waterbodiesWithLatestData = await Promise.all(waterbodies.map(async (waterbody) => {
+            const latestCellCount = await getLatestCellCount(waterbody._id);
+            const latestWaterDescription = await getLatestWaterDescription(waterbody._id);
+            return {
+                ...waterbody.toObject(),
+                latestCellCount,
+                latestWaterDescription
+            };
+        }));
+        res.json(waterbodiesWithLatestData);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: 'Server error' });
+    }
 });
 
 module.exports = router;
